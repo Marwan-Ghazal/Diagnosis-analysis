a63146d12d3037829744583fbdf105516fc5fc20
marwan-ghazal <marwanghazal002y@gmail.com>
2026-03-28 19:34:46 +0200

    adding png's in summary

--- summary.sh
+++ summary.sh
@@ -8,6 +8,9 @@ docker cp pipeline-container:/app/pipeline/insight2.txt results/
 docker cp pipeline-container:/app/pipeline/insight3.txt results/
 docker cp pipeline-container:/app/pipeline/summary_plot.png results/
 docker cp pipeline-container:/app/pipeline/clusters.txt results/
+docker cp pipeline-container:/app/pipeline/pairplot.png results/
+docker cp pipeline-container:/app/pipeline/heatmap.png results/
+docker cp pipeline-container:/app/pipeline/histogram.png results/
 
 echo "saved outputs to results/"
 
